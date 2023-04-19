3c1041e317c964a3ee84f920afdd95bc3b2b822c
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-04-19 17:59:41 +0530

    task 3 done

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
 document.querySelector('#main-header').style.borderBottom="3px solid black";
 const add=document.getElementsByClassName('title');
 add[0].style.fontWeight="bold"; 
-add[0].style.color="green"; 
+add[0].style.color="green";

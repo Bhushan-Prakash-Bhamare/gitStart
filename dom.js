433ee8546269bc4843e6936cb9a45a851605dd29
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-04-20 16:45:50 +0530

    task 4

--- dom.js
+++ dom.js
@@ -1,4 +1,10 @@
 document.querySelector('#main-header').style.borderBottom="3px solid black";
-const add=document.getElementsByClassName('title');
-add[0].style.fontWeight="bold"; 
-add[0].style.color="green"; 
+    const add=document.getElementsByClassName('title');
+    add[0].style.fontWeight="bold"; 
+    add[0].style.color="green"; 
+    const list=document.getElementsByClassName('list-group-item');
+    list[2].style.backgroundColor='green';
+    for(let i=0;i<list.length;i++)
+    {
+        list[i].style.fontWeight='bold';
+    }

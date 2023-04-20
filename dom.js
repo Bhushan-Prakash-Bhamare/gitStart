a7b363a3f38bffed66ff788a7573a1cee3213afd
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-04-20 18:11:29 +0530

    task6

--- dom.js
+++ dom.js
@@ -1,28 +1,11 @@
-document.querySelector('#main-header').style.borderBottom="3px solid black";
-    const add=document.getElementsByClassName('title');
-    add[0].style.fontWeight="bold"; 
-    add[0].style.color="green"; 
-    const list=document.getElementsByClassName('list-group-item');
-    list[2].style.backgroundColor='green';
-    for(let i=0;i<list.length;i++)
-    {
-        list[i].style.fontWeight='bold'; 
-    }
-    // const newli=document.getElementsByClassName("list-new");
-    // newli[0].style.backgroundColor='yellow';
-    // newli[0].style.fontWeight='bold';
-    // newli[0].style.height='50px';
-    // newli[0].style.border='1px solid grey';
-    // newli[0].style.padding='13px';
-    // newli[0].style.paddingLeft='20px';
-    // newli[0].textContent="Item 5";
-    // newli[0].style.listStyle='none';
-    const tagname=document.getElementsByTagName('li');
-    tagname[4].style.backgroundColor='yellow';
-    tagname[4].style.fontWeight='bold';
-    tagname[4].style.height='50px';
-    tagname[4].style.border='1px solid grey';
-    tagname[4].style.padding='13px';
-    tagname[4].style.paddingLeft='20px';
-    tagname[4].textContent="Item 5";
-    tagname[4].style.listStyle='none';
+// const item2=document.querySelector('.list-group');
+// console.log(item2);
+// item2.children[1].style.backgroundColor='green'
+// item2.children[2].style.display='none';
+const list1=document.querySelectorAll('.list-group-item');
+list1[1].style.color='green';
+var listodd=document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(let i=0;i<listodd.length;i++)
+{
+    listodd[i].style.backgroundColor='green';
+}

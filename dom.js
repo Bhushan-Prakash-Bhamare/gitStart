ec99320c6a625240c4374b18f9b58b698c2c84c8
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-04-22 09:58:26 +0530

    task 13

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
 let forms=document.getElementById('my-form');
 forms.addEventListener('submit',addData);
 
+
 function addData(e)
 {
     e.preventDefault();
@@ -12,4 +13,28 @@ function addData(e)
     } 
     let myobj_s=JSON.stringify(myobj);
     localStorage.setItem(emailInput,myobj_s);
+    showUser(myobj);
+}
+function showUser(myobj)
+{
+    var parentElem=document.getElementById('users');
+    const childElem=document.createElement('li');
+   // childElem.className='del-list';
+    childElem.textContent=myobj.name+'-'+myobj.email+" "; 
+    const deletebtn=document.createElement('button');
+   // deletebtn.className='btn1';
+    deletebtn.textContent='Delete';
+    childElem.appendChild(deletebtn);
+    parentElem.appendChild(childElem);
+
+    deletebtn.addEventListener('click',function(){
+        removeItem(event,myobj,parentElem);
+    });
+}
+
+function removeItem(e,myobj,parentElem)
+{
+            let item=e.target.parentElement;
+            parentElem.removeChild(item); 
+            localStorage.removeItem(myobj.email);
 }
